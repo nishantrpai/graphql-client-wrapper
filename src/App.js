0f85e8b5a2c01731bf0f1479b7adcac248a5139f
nishantrpai <eiglesias914@gmail.com>
2019-08-27 15:54:07 +0530

    unused packages are removed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Posts from './containers/Posts';
 import Post from './containers/Post';
-import { getAllPosts } from './services';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
 class App extends React.Component {
